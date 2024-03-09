9681b48ec360b3ddb87eee083a9bd0476dd16972
Edwar Amaya <edwar.amaya@ppm.com.ec>
2024-03-09 02:39:15 -0500

    Change grid design

--- src/components/ui/GridBackground.tsx
+++ src/components/ui/GridBackground.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 export function GridBackground({ children }: Props) {
   return (
-    <div className="min-h-screen bg-transparent bg-dot-white/[0.2]">
+    <div className="min-h-screen bg-transparent bg-dot-white/[0.15]">
       <div className="max-w-7xl mx-auto p-5">
         {children}
       </div>
