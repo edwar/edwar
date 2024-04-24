60fab287c9d9dc74ee51422f7a8b9b66e4f9b952
Edwar Amaya <edwar.amaya@ppm.com.ec>
2024-04-24 15:21:29 -0500

    No print command

--- src/app/cv/components/KeyboardManager.tsx
+++ src/app/cv/components/KeyboardManager.tsx
@@ -73,8 +73,8 @@ export default function KeyboardManager() {
     : actions;
 
   return (
-    <div className="no-print">
-      <div className="fixed inset-0 py-4 h-14 w-28 lg:inline-flex hidden gap-1 left-1/2 -translate-x-1/2">
+    <>
+      <div className="no-print fixed inset-0 py-4 h-14 w-28 lg:inline-flex hidden gap-1 left-1/2 -translate-x-1/2">
         <kbd className="flex justify-center items-center gap-1 w-14 px-2 py-1.5 text-xs font-semibold border rounded-lg bg-gray-600 text-gray-100 border-gray-500"><BsCommand size={15} />Cmd</kbd>
         <kbd className="flex justify-center items-center gap-1 w-7 px-2 py-1.5 text-xs font-semibold border rounded-lg bg-gray-600 text-gray-100 border-gray-500">+</kbd>
         <kbd className="flex justify-center items-center gap-1 w-7 px-2 py-1.5 text-xs font-semibold border rounded-lg bg-gray-600 text-gray-100 border-gray-500">K</kbd>
@@ -125,6 +125,6 @@ export default function KeyboardManager() {
           ))}
         </CommandList>
       </Command>
-    </div>
+    </>
   );
 }
