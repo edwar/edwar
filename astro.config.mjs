85aa0b5682ccb419b6efef216090879a842e78e8
Edwar Amaya <edwaramayadiaz@gmail.com>
2025-08-11 00:31:08 -0500

    fix: add sharp dependency for image processing

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,17 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+      config: {
+        limitInputPixels: false,
+      },
+    },
+  },
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [icon()]
+  integrations: [icon({})]
 });
